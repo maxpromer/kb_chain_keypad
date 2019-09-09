c6720ccdb97acf61016bd13f08872442fbf50443
Max <max30012540@hotmail.com>
2019-09-09 09:30:42 +0700

    Update en.js

--- pcf8574/msg/en.js
+++ pcf8574/msg/en.js
@@ -1,8 +1,11 @@
-Blockly.Msg.MPL3115A2_READ_PRESSURE_MESSAGE = "Read pressure";
-Blockly.Msg.MPL3115A2_READ_PRESSURE_TOOLTIP = "";
+Blockly.Msg.KB_CHAIN_KEYPAD_ON_PESS_MESSAGE = "Keypad on pess %1 %2";
+Blockly.Msg.KB_CHAIN_KEYPAD_ON_PESS_TOOLTIP = "";
 
-Blockly.Msg.MPL3115A2_READ_ALTITUDE_MESSAGE = "Read altitude";
-Blockly.Msg.MPL3115A2_READ_ALTITUDE_TOOLTIP = "";
+Blockly.Msg.KB_CHAIN_KEYPAD_ON_RELE_MESSAGE = "Keypad on release %1 %2";
+Blockly.Msg.KB_CHAIN_KEYPAD_ON_RELE_TOOLTIP = "";
 
-Blockly.Msg.MPL3115A2_READ_TEMPERATURE_MESSAGE = "Read temperature";
-Blockly.Msg.MPL3115A2_READ_TEMPERATURE_TOOLTIP = "";
+Blockly.Msg.KB_CHAIN_KEYPAD_GET_KEY_MESSAGE = "Keypad get key (Char)";
+Blockly.Msg.KB_CHAIN_KEYPAD_GET_KEY_TOOLTIP = "";
+
+Blockly.Msg.KB_CHAIN_KEYPAD_GET_KEY_STRING_MESSAGE = "Keypad get key (String)";
+Blockly.Msg.KB_CHAIN_KEYPAD_GET_KEY_STRING_TOOLTIP = "";
